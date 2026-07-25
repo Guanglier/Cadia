102de5f598ef71bc97fc33bb058b0eb3547ccb56
Cyrile.MONANGE@bdrthermea.fr <Cyrile.MONANGE@bdrthermea.fr>
2026-07-25 16:05:20 +0200

    fdsfs

--- build/Desktop_MSVC_2022_amd64_debug-Debug/.qt/deploy_Cad_IA_5a2c613d1b.cmake
+++
@@ -1,8 +0,0 @@
-include("C:/__Perso/test_prog/Cadia/trunk/build/Desktop_MSVC_2022_amd64_debug-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/Cad_IA-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "C:/__Perso/test_prog/Cadia/trunk/build/Desktop_MSVC_2022_amd64_debug-Debug/Cad_IA.exe"
-    GENERATE_QT_CONF
-)
